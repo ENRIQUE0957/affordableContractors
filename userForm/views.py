e2c5a6e41bee24faf2a83b64792ee6e1b3c72ffb
enrique cipriano <enriquecipriano0957@yahoo.com>
2021-10-28 18:28:50 -0500

    changes
    chnages

--- userForm/views.py
+++ userForm/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from . import forms
-# Create your views here.
+from django.core.mail import send_mail
 def affordableContractors(request):
     form = forms.UserInput()
     if request.method == 'POST':
         form=forms.UserInput(request.POST)
         if form.is_valid():
+            name = request.POST['name']
+            email = request.POST['email']
             #what we want to happen
             form.save(commit=True)
+            send_mail(
+                #subject
+                name,
+                #message
+                email,
+                #from email
+                email,
+                #to mail
+                [email,email],
+            )
 
         else:
             print('Sorry no post request found')
