a3fbd0d30951507b7d89c99f119eb6fdcea719bf
enrique cipriano <enriquecipriano0957@yahoo.com>
2021-11-10 21:31:33 -0600

    chnages
    chnages

--- userForm/models.py
+++ userForm/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class UserInfo(models.Model):
     name = models.CharField(max_length=260)
-    email = models.EmailField()
-    verify = models.EmailField()
+    email=models.EmailField()
+    phone=models.IntegerField(max_length=264)
