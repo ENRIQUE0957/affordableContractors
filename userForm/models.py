c38b93d7f4424d039e74277322519fa219c0dde2
enrique cipriano <enriquecipriano0957@yahoo.com>
2021-11-17 17:42:18 -0600

    Card-details
    card-details

--- userForm/models.py
+++ userForm/models.py
@@ -3,5 +3,4 @@ from django.db import models
 # Create your models here.
 class UserInfo(models.Model):
     name = models.CharField(max_length=260)
-    email=models.EmailField()
-    phone=models.IntegerField(max_length=264)
+    email = models.EmailField()
